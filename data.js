9a3445d54acbd23d403d23f5f14664e9a54be871
joVal21 <joseph.valentin21@gmail.com>
2021-03-07 08:11:41 -0500

    CHANGES MADE TO find_player func

--- data.js
+++ data.js
@@ -1,34 +1,39 @@
  //document.write(player_name)
  //document.getElementById("output").innerHTML = player_name
  //console.log(player_name)'
+ var totalStats = []
+
+ totalStats.push({
+     player: "Mike",
+     Runs: 30
+ })
+ totalStats.push({
+     player: "Ken",
+     Runs: 60
+ })
+
+console.log(totalStats)
+
+
 
  function playerSearch() {
+
      var player_name = document.getElementById("playerName").value.toString()
 
      document.getElementById("output").innerHTML = find_player(player_name)
      //document.getElementById("output").innerText = find_player(player_name)
+
  }
 
- var totalStats = []
+ function find_player(user){
+     //var result = totalStats.filter(x => user.toLowerCase() == x["player"].toLowerCase())
+    var output = ""
+     for (var i = 0; i < totalStats.length;i++){
+         if (user == totalStats[i]["player"])
+            output = totalStats[i]["player"] + " scored "+ totalStats[i]["Runs"] + " runs!!!"
 
- totalStats.push({
-     Player: "Mike Trout",
-     Runs: 30
- })
- totalStats.push({
-     Player: "Ken Griffey",
-     Runs: 60
- })
- console.log(totalStats)
-
-
- function find_player(user_input) {
-     for (player in totalStats) {
-         if (user_input == player["Player"]) {
-             console.log(player)
-             return player["Player"]
-         }
      }
-
+     return output
  }
+
  //console.log(find_player("Mike Trout"))
