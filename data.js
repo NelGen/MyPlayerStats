7e2debd563d662bef607ccd517f03a8e999ca0bb
joVal21 <joseph.valentin21@gmail.com>
2021-03-06 20:00:34 -0500

    JAVA and HTML Updates

--- data.js
+++ data.js
@@ -1,10 +1,34 @@
+ //document.write(player_name)
+ //document.getElementById("output").innerHTML = player_name
+ //console.log(player_name)'
 
-//document.write(player_name)
-//document.getElementById("output").innerHTML = player_name
-//console.log(player_name)'
+ function playerSearch() {
+     var player_name = document.getElementById("playerName").value.toString()
 
-function playerSearch(){
-    var player_name = document.getElementById("playerName").value
-    document.getElementById("output").innerHTML = player_name
-    console.log(player_name)
-}
+     document.getElementById("output").innerHTML = find_player(player_name)
+     //document.getElementById("output").innerText = find_player(player_name)
+ }
+
+ var totalStats = []
+
+ totalStats.push({
+     Player: "Mike Trout",
+     Runs: 30
+ })
+ totalStats.push({
+     Player: "Ken Griffey",
+     Runs: 60
+ })
+ console.log(totalStats)
+
+
+ function find_player(user_input) {
+     for (player in totalStats) {
+         if (user_input == player["Player"]) {
+             console.log(player)
+             return player["Player"]
+         }
+     }
+
+ }
+ //console.log(find_player("Mike Trout"))
