c76bac3c7204d3b772987ee7d399a327d6d7b856
joVal21 <joseph.valentin21@gmail.com>
2021-03-06 19:20:29 -0500

    INIT COMMIT

---
+++ data.js
@@ -0,0 +1,10 @@
+
+//document.write(player_name)
+//document.getElementById("output").innerHTML = player_name
+//console.log(player_name)'
+
+function playerSearch(){
+    var player_name = document.getElementById("playerName").value
+    document.getElementById("output").innerHTML = player_name
+    console.log(player_name)
+}
